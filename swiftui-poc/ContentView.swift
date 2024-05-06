016774d52fd05a007c553c41dbc3d5987cc415f4
Rodolfo Ramírez <rodolfo.cedillo@cbrands.com>
2024-05-06 09:15:05 -0600

    Initial Commit

---
+++ swiftui-poc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-poc
+//
+//  Created by Rodolfo Ramírez Cedillo on 06/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-poc/swiftui_pocApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_pocApp.swift
+//  swiftui-poc
+//
+//  Created by Rodolfo Ramírez Cedillo on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_pocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
